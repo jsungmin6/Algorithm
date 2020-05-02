4442585a7b48b3d431e4e4804f0bfc9de0f38e59
KimTaiMin <jsungmin506@gmail.com>
2020-05-02 22:33:40 +0900

    if( in ) 이 아니라 include 를 사용해야 했음 그런데 체육복여벌있는 사람이 잃어버리는 조건을 안 읽어서 오류

--- Programmers/Javascript/level1/체육복.js
+++ Programmers/Javascript/level1/체육복.js
@@ -1,19 +1,24 @@
-var n=5, lost=[2,4],reserve=[1,3,5];
+var n=5, lost=[2,3],reserve=[3,4];
 
 function solution(n, lost, reserve) {
     var answer=n-lost.length;
     var count=0;
     for (var i of lost)
         {
-            if(i-1 in reserve)
+            if(reserve.includes(i-1))
                 {
+                  console.log('i-1',i-1)
                     count+=1;
                     reserve.splice(reserve.indexOf(i-1),1);
-                } else if(i+1 in reserve){
+                    console.log(reserve)
+                } else if(reserve.includes(i+1)){
+                  console.log('i+1',i+1)
                     count+=1;
                     reserve.splice(reserve.indexOf(i+1),1);
+                    console.log(reserve)
                 }
         }
     return answer+count
 }
+
 console.log(solution(n, lost, reserve));
