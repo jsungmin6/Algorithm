c9d645de46c080edaee68a6d121f1dc94ef2161b
KimTaiMin <jsungmin506@gmail.com>
2020-05-06 22:15:33 +0900

    통과된 코드... 무슨 차인지 모르겠다.

--- Programmers/Javascript/level1/실패율.js
+++ Programmers/Javascript/level1/실패율.js
@@ -1,40 +1,27 @@
-var N=15;
-var stages=[12,12,12,12,12,6,6]
-
+var N=5;
+var stages=[2, 1, 2, 6, 2, 4, 3, 3]
 function solution(N, stages) {
-    var all=stages.length
-    var temp=[]
-    for(var i=1;i<=N;i++)
-        {
-            if(all!=0){
-                var ct=count(i,stages);
-                temp.push([i,ct/all])
-                all-=ct
-            } else{
-              temp.push([i,0])
-            }
+    let ans = []
 
+    for (let i = 1; i <= N; ++i) {
+        let usersReachedCurrentStage   = stages.reduce((acc, curStage) => acc + ((curStage >= i) ? 1 : 0), 0)
+        console.log("usersReachedCurrentStage : " + usersReachedCurrentStage)
+        let usersStagnatedCurrentStage = stages.reduce((acc, curStage) => acc + ((curStage == i) ? 1 : 0), 0)
+        console.log("usersStagnatedCurrentStage : " + usersStagnatedCurrentStage)
+        if (usersReachedCurrentStage === 0) {
+            ans.push({ 'stage': i, 'failRate': 0 })
+            console.log(ans)
+            continue
         }
-    console.log(temp)
-    var answer=temp.sort(function(x,y){if(x[1]===y[1]){return 1;}
-    return y[1]-x[1]})
-    console.log(answer)
-    var list=[];
-    for(var i of answer){
-      list.push(i[0])
+
+        ans.push({ 'stage': i, 'failRate': (usersStagnatedCurrentStage / usersReachedCurrentStage) })
+        console.log(ans)
     }
-    return list
-}
 
-console.log(solution(N,stages))
-
-function count(num,list){
-    var count=0
-    for(var i of list)
-        {
-            if(i===num){
-                count+=1
-            }
-        }
-    return count
+    return ans.sort((a, b) => {
+        if (a.failRate > b.failRate) return -1
+        if (a.failRate < b.failRate) return 1
+        return a.stage - b.stage
+    }).map(entry => entry.stage)
 }
+solution(N,stages)
