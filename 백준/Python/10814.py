ea0885f2ba07b2bf6ca5ddd2f8f50a4403aad692
KimTaiMin <jsungmin506@gmail.com>
2020-05-13 22:24:31 +0900

    제대로된 정답

--- 백준/Python/10814.py
+++ 백준/Python/10814.py
@@ -1,12 +1,13 @@
 n=int(input());
 array=[]
 for i in range(n):
-    array.append(list(input().split(' ')))
+    temp=input().split(' ')
+    array.append((int(temp[0]),temp[1]))
 
 answer=sorted(array,key=lambda x:x[0])
 
-for i in range(len(answer)):
-    print(i)
+for i in answer:
+    print(i[0],i[1])
 
 
 
