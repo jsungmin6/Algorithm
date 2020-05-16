a894928065d369d4f803dce9df04b741b20b1e17
KimTaiMin <jsungmin506@gmail.com>
2020-05-16 21:31:56 +0900

    나의풀이

---
+++ 백준/Python/1668.py
@@ -0,0 +1,23 @@
+n=int(input())
+array=[]
+left=1
+right=1
+index=0
+for _ in range(n):
+    array.append(int(input()))
+
+for i in range(0,len(array)-1):
+    if(array[index]<array[i+1]):
+        left+=1
+        index=i+1
+
+index=len(array)-1
+for i in range(len(array)-1,0,-1):
+    if(array[index]<array[i-1]):
+        right+=1
+        index=i-1
+
+
+print(left,right)
+
+    
