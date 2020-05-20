27c23cd169788b67f85d961fafbc69c66d2e6973
KimTaiMin <jsungmin506@gmail.com>
2020-05-20 13:32:27 +0900

    무슨 차인지 모르겠다.

--- 백준/Python/1927.py
+++ 백준/Python/1927.py
@@ -2,14 +2,18 @@ import heapq
 
 n=int(input())
 heap=[]
+result=[]
 for _ in range(n):
     data=int(input())
     if data==0:
         if heap:
-            print(heapq.heappop(heap))
+            result.append(heapq.heappop(heap))
         else:
-            print(0)
+            result.append(0)
     else:
         heapq.heappush(heap,data)
+
+for i in result:
+    print(i)
 
     
