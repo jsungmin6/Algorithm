6eb0c4d753385964043c91ea1a18e513b77bc459
KimTaiMin <jsungmin506@gmail.com>
2020-05-20 16:03:49 +0900

    에러

--- 백준/Python/1766.py
+++ 백준/Python/1766.py
@@ -1,26 +1,18 @@
 import heapq
 
-n=6
-m=3
-
+n,m = map(int,input().split())
 array=[[] for i in range(n+1)]
 indegree = [0]*(n+1)
 
-heap=[]
-result=[]
-
-data=[(3,1),(4,2),(6,5)]
-for x,y in data:
+for _ in range(m):
+    x,y = map(int,input().split())
     array[x].append(y)
     indegree[y]+=1
 
+heap=[]
 for i in range(1,n+1):
     if indegree[i]==0:
         heapq.heappush(heap,i)
-
-print(array)
-print(indegree)
-print(heap)
 
 result=[]
 
@@ -28,8 +20,8 @@ while heap:
     data = heapq.heappop(heap)
     result.append(data)
     for y in array[data]:
-        indegree[y] -=1
-        if indegree[y] ==0:
+        indegree[y]-=-1
+        if indegree[y]==0:
             heapq.heappush(heap,y)
 
 for i in result:
