51cfe7735f7f09d0ef10063118df2c0c14b534e2
KimTaiMin <jsungmin506@gmail.com>
2020-11-05 13:50:30 +0900

    알고파 최단경로 다익스트라

--- 백준/Python/알고파/최단경로/1753.py
+++ 백준/Python/알고파/최단경로/1753.py
@@ -2,28 +2,28 @@ import collections
 import heapq
 import sys
 
-input=sys.stdin.readline
+input = sys.stdin.readline
 
-V,E = map(int,input().split())
+V, E = map(int, input().split())
 K = int(input())
-datas = [list(map(int,input().split())) for _ in range(E)]
+datas = [list(map(int, input().split())) for _ in range(E)]
 
-graph =collections.defaultdict(list)
-for u,v,w in datas:
-    graph[u].append((v,w))
+graph = collections.defaultdict(list)
+for u, v, w in datas:
+    graph[u].append((v, w))
 
-Q=[(0,K)]
+Q = [(0, K)]
 dist = collections.defaultdict(int)
 
 while Q:
     time, node = heapq.heappop(Q)
     if node not in dist:
         dist[node] = time
-        for v,w in graph[node]:
-            alt = time + w;
-            heapq.heappush(Q,(alt,v))
+        for v, w in graph[node]:
+            alt = time + w
+            heapq.heappush(Q, (alt, v))
 
-for i in range(1,V+1):
+for i in range(1, V+1):
     if i == K:
         print(0)
     else:
@@ -31,3 +31,58 @@ for i in range(1,V+1):
             print('INF')
         else:
             print(dist[i])
+
+# 블루코드
+
+input = sys.stdin.readline
+V, E = map(int, input().split())
+K = int(input())
+datas = [list(map(int, input().split())) for _ in range(E)]
+graph = [[] for _ in range(20005)]
+for u, v, w in datas:
+    graph[u].append((v, w))
+Q = [(0, K)]
+dist = [987654321 for i in range(20005)]
+dist[K] = 0
+while Q:
+    time, node = heapq.heappop(Q)
+    for v, w in graph[node]:
+        if dist[v] > dist[node] + w:
+            heapq.heappush(Q, (dist[node] + w, v))
+            dist[v] = dist[node] + w
+for i in range(1, V+1):
+    if dist[i] == 987654321:
+        print('INF')
+    else:
+        print(dist[i])
+
+# 블루코드 개선
+import heapq
+import sys
+input = sys.stdin.readline
+V, E = map(int, input().split())
+K = int(input())
+datas = [list(map(int, input().split())) for _ in range(E)]
+graph = [[] for _ in range(20005)]
+for u, v, w in datas:
+    graph[u].append((v, w))
+Q = [(0, K)]
+dist = [987654321 for i in range(20005)]
+while Q:
+    time, node = heapq.heappop(Q)
+
+    if dist[node] != 987654321:
+        continue
+
+    dist[node] = time
+
+    for v, w in graph[node]:
+        if dist[v] != 987654321:
+            continue
+        heapq.heappush(Q, (dist[node] + w, v))
+
+for i in range(1, V+1):
+    if dist[i] == 987654321:
+        print('INF')
+    else:
+        print(dist[i])
---
+++ 백준/Python/알고파/최단경로/18352.py
@@ -0,0 +1,44 @@
+# 특정 거리의 도시 찾기
+
+# 풀이 과정
+'''
+모든 도시의 최단거리를 찾고 일치하는 최단거리를 출력하는 것이기 때문에
+다익스트라 알고리즘을 사용해서 출발지점으로부터 모든 도시의 최단거리를 찾는다.
+'''
+import sys
+import collections
+import heapq
+input = sys.stdin.readline
+
+N, M, K, X = map(int, input().split())
+datas = [list(map(int, input().split())) for _ in range(M)]
+
+graph = collections.defaultdict(list)
+
+for u, v in datas:
+    graph[u].append(v)
+
+
+dist = collections.defaultdict(int)
+
+Q = [(0, X)]
+
+answer = []
+
+while Q:
+    weight, node = heapq.heappop(Q)
+    if node not in dist:
+        if weight == K:
+            heapq.heappush(answer, node)
+        dist[node] = weight
+
+        for v in graph[node]:
+            alt = weight+1
+            if alt < dist[v]:
+                heapq.heappush(Q, (alt, v))
+
+if not answer:
+    print(-1)
+else:
+    for i in range(len(answer)):
+        print(heapq.heappop(answer))
