228749e24c46b058d0338c43363bde70a762d4f3
jsungmin6 <jsungmin506@gmail.com>
2021-01-16 23:26:09 +0900

    우선순위 큐

--- 백준/Python/카테고리/우선순위 큐/1715(카드 정렬하기).py
+++ 백준/Python/카테고리/우선순위 큐/1715(카드 정렬하기).py
@@ -7,7 +7,7 @@
 sort로 정렬해 풀면 큐보다 시간이 더 걸린다. 왜냐하면 합친 결과를 다시 리스트에 넣어야 하기 때문.
 
 풀이과정
-우선순위 큐에 데이터를 넣고 두개씩 꺼낸후 결과를 다시 큐에 push한다. 마지막 남은게 정답
+우선순위 큐에 데이터를 넣고 두개씩 꺼낸후 결과를 다시 큐에 push한다. 힙에서 나온 숫자들을 다 더한게 정답.
 
 시간복잡도
 O(NlogN)
@@ -22,12 +22,8 @@ for _ in range(N):
 
 ans=0
 while len(heap) != 1:
-    print(heap)
-    a=heapq.heappop(heap)
-    b=heapq.heappop(heap)
-
-    ans+=a+b
-
-    heapq.heappush(heap,a+b)
+    n=heapq.heappop(heap) + heapq.heappop(heap)
+    ans+=n
+    heapq.heappush(heap,n)
 
 print(ans)
