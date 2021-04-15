8bc137bce66e26e76ff31dca4be0a33f2a6aa0d7
jsungmin6 <jsungmin506@gmail.com>
2021-04-16 00:43:05 +0900

    에라토스테네스의 체

---
+++ 백준/Python/카테고리/에라토스테네스의 체/1929(소수 구하기).py
@@ -0,0 +1,39 @@
+'''
+풀이
+에라토스 테네스의 체
+'''
+M,N = map(int,input().split())
+# def era_prime (M,N):
+#     check, answer = [0 for i in range(N+1)], []
+#     for i in range(2, N+1):
+#         if check[i] == 0:
+#             if i >= M: answer.append(i)
+#         else:
+#             continue
+#         for j in range(i*i, N+1, i):
+#             check[j] = 1
+#     return answer
+check, answer = [0 for i in range(N+1)], []
+# check, answer = [0 for i in range(N+1)], []
+for i in range(2,N+1):
+    if check[i] == 0:
+        if i >= M: answer.append(i)
+    else:
+        continue
+    for j in range(i*i, N+1,i):
+        check[j] = 1
+
+for i in answer:
+    print(i)
+
+# M,N = map(int,input().split())
+# visit = [0]*(N+1)
+# visit[0],visit[1] = 1,1
+# for i in range(2,N+1):
+#     if visit[i]: continue
+#     for j in range(i*i,N+1,i):
+#         visit[j] = 1
+
+# for i in range(M,N+1):
+#     if not visit[i]:
+#         print(i)
